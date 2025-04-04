50a2cba735c5fe6623227ce1c62e6ca225e872ee
Alexey <alexey.nikonov00@mail.ru>
2025-04-04 20:20:10 +0000

    Separated database fill functions

--- db_fill.py
+++ db_fill.py
@@ -1,5 +1,6 @@
 import os
 import django
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onlineshop.settings")
 django.setup()
 
@@ -14,10 +15,8 @@ from faker import Faker
 import random
 
 
-
-def handle():
+def fill_users_table():
     fake = Faker()
-    '''
     users = []
     for i in range(5):
         user = User.objects.create(
@@ -27,43 +26,56 @@ def handle():
             username=fake.name(),
         )
         users.append(user)
-        print(f'Created user with login {user.login}, email {user.email}, password {user.password}, username {user.username}')
+        print(
+            f'Created user with login {user.login}, email {user.email}, password {user.password}, username {user.username}')
 
+
+def fill_orders_table():
+    fake = Faker()
     orders = []
     for i in range(15):
         order = Order.objects.create(
-            user_id = User.objects.get(id=random.randint(1,4)),
-            status = Order.Status.DRAFT,
-            address = fake.city(),
-            #delivery_fee = 1.0
+            user_id=User.objects.get(id=random.randint(1, 4)),
+            status=Order.Status.DRAFT,
+            address=fake.city(),
+            delivery_fee=1.0
         )
         orders.append(order)
         print(f'Created order with id {order.order_id}, belongs to user {order.user_id}, address {order.address}')
 
+
+def fill_products_table():
+    fake = Faker()
     products = []
     for i in range(20):
         product = Products.objects.create(
-            product_name=fake.city(),
+            product_name=fake.word(),
             price=random.randint(10, 100),
             stored_amount=random.randint(1, 10),
         )
         products.append(product)
-        print(f'Create product {product.product_name} with id {product.product_id}, price {product.price}, stored {product.stored_amount}')
+        print(
+            f'Create product {product.product_name} with id {product.product_id}, price {product.price}, stored {product.stored_amount}')
 
+
+def fill_order_products_table():
+    fake = Faker()
     order_products = []
-    for i in range (30):
+    for i in range(30):
         order_product = OrderProducts.objects.create(
-            order_id = Order.objects.get(order_id=max(int(i/2), 1)),
-            product_id = Products.objects.get(product_id=random.randint(1, 20)),
-            amount = random.randint(1, 15)
+            order_id=Order.objects.get(order_id=max(int(i / 2), 1)),
+            product_id=Products.objects.get(product_id=random.randint(1, 20)),
+            amount=random.randint(1, 15)
         )
         order_products.append(order_product)
         print(f'Added {order_product.amount} products {order_product.product_id} to {order_product.order_id}')
 
+
+def handle():
     print('Show email of users which have orders with id in [2, 7, 9]')
-    emails = User.objects.filter(order__order_id__in=[2,7,9]).values('email')
+    emails = User.objects.filter(order__order_id__in=[2, 7, 9]).values('email')
     print(emails)
-'''
+
     print('Show order products grouped by user')
     user = User.objects.get(id=1)
     ords = Order.objects.filter(user_id=1)
@@ -79,4 +91,3 @@ def handle():
     print(f'User with id {1} have orders:')
     for order in order_info:
         print(f'order id {order[0]}, contains products wwith id\'s {order[1][0]}')
-handle()
